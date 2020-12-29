3263f72519c9364d91715bbf6d1924eb47c41a62
zhaozhimin <zhaozhimin@chinaiptv.cn>
2020-12-29 16:56:23 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { createApp } from 'vue'
+/*
+ * @Author: zhimin
+ * @Date: 2020-12-29 10:04:54
+ * @LastEditors: zhimin
+ * @LastEditTime: 2020-12-29 15:56:04
+ * @FilePath: \v-3\chap08\jingdong\src\main.js
+ */
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+import './style/index.css'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
+<!--
+ * @Author: zhimin
+ * @Date: 2020-12-29 10:04:54
+ * @LastEditors: zhimin
+ * @LastEditTime: 2020-12-29 10:11:33
+ * @FilePath: \v-3\chap08\jingdong\src\views\Home.vue
+-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">this is home page</div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
